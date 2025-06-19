a616cd2c445694d362246f5c6312461d829f4c8a
ySsio <88093363+ySsio@users.noreply.github.com>
2025-06-19 18:07:13 +0900

    feat: Texture LOD
    - Deferred Shader texture sampling 거리에 따른 LOD 적용.
    - 다른 anti aliasing 적용 후 수치 재조정 필요

--- Source/Engine/Shader/std3d_deferred.fx
+++ Source/Engine/Shader/std3d_deferred.fx
@@ -127,15 +127,22 @@ PS_OUT PS_Std3D_Deferred(VS_OUT _in)
 
     float4 vColor = float4(1.f, 0.f, 1.f, 1.f);
     float3 vNormal = _in.vViewNormal;
+    
+	const float maxLOD = 6.f;
+	const float threshold = 1200.f;
+    float dist = clamp(length(_in.vViewPos), 0.f, threshold) / threshold;
+	//float dist = saturate(length(_in.vViewPos) / threshold);
+    
+	const float level = dist * maxLOD;
 
     if (g_btex_0)
     {
-        vColor = g_tex_0.Sample(g_sam_0, _in.vUV);
-    }
+		vColor = g_tex_0.SampleLevel(g_sam_0, _in.vUV, level);
+	}
 
     if (g_btex_1)
     {
-        vNormal = g_tex_1.Sample(g_sam_0, _in.vUV).xyz;
+		vNormal = g_tex_1.SampleLevel(g_sam_0, _in.vUV, level).xyz;
 
         // 추출한 색상값(0~1 범위) 을 방향벡터 값의 범위(-1 ~ 1) 로 변경한다.
         vNormal = (vNormal * 2.f) - 1.f;
@@ -154,11 +161,10 @@ PS_OUT PS_Std3D_Deferred(VS_OUT _in)
 
     if(g_btex_2)
     {
-        float4 vSpefCoeffic = g_tex_2.Sample(g_sam_0, _in.vUV);
+		float4 vSpefCoeffic = g_tex_2.SampleLevel(g_sam_0, _in.vUV, level);
+	}
 
-    }
-
-    output.Color = vColor;
+	output.Color = vColor;
     output.Normal = float4(vNormal, 1.f);
     output.Position = float4(_in.vViewPos, 1.f);
     output.Emissive = (float4) 0.f;
--- Source/Engine/System/Private/Rendering/Device/CDevice.cpp
+++ Source/Engine/System/Private/Rendering/Device/CDevice.cpp
@@ -173,7 +173,7 @@ int CDevice::CreateRasterizerState()
 	D3D11_RASTERIZER_DESC Desc = {};
 	Desc.CullMode = D3D11_CULL_BACK;
 	Desc.FillMode = D3D11_FILL_SOLID;
-	Desc.ScissorEnable = true;	// TEST : Scissor
+	Desc.ScissorEnable = true;
 	DEVICE->CreateRasterizerState(
 		&Desc, m_RSState[static_cast<UINT>(RS_TYPE::CULL_BACK)].GetAddressOf());
 
@@ -182,21 +182,21 @@ int CDevice::CreateRasterizerState()
 	// CULL_FRONT
 	Desc.CullMode = D3D11_CULL_FRONT;
 	Desc.FillMode = D3D11_FILL_SOLID;
-	Desc.ScissorEnable = true;	// TEST : Scissor
+	Desc.ScissorEnable = true;
 	DEVICE->CreateRasterizerState(
 		&Desc, m_RSState[static_cast<UINT>(RS_TYPE::CULL_FRONT)].GetAddressOf());
 
 	// CULL_NONE
 	Desc.CullMode = D3D11_CULL_NONE;
 	Desc.FillMode = D3D11_FILL_SOLID;
-	Desc.ScissorEnable = true;	// TEST : Scissor
+	Desc.ScissorEnable = true;
 	DEVICE->CreateRasterizerState(
 		&Desc, m_RSState[static_cast<UINT>(RS_TYPE::CULL_NONE)].GetAddressOf());
 
 	// WIRE_FRAME
 	Desc.CullMode = D3D11_CULL_NONE;
 	Desc.FillMode = D3D11_FILL_WIREFRAME;
-	Desc.ScissorEnable = true;	// TEST : Scissor
+	Desc.ScissorEnable = true;
 	DEVICE->CreateRasterizerState(
 		&Desc, m_RSState[static_cast<UINT>(RS_TYPE::WIRE_FRAME)].GetAddressOf());
 
